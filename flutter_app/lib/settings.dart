4d6e21c7338f860ed66f08f8457f3806847521a0
Armont52 <102473445+Armont52@users.noreply.github.com>
2025-04-07 04:15:51 +0900

    MyPills, Search by name
    나의 복용약 페이지,
    제품명/코드로 검색 기능 구현

--- flutter_app/lib/settings.dart
+++ flutter_app/lib/settings.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
   static const String drugInfoUrl = 'http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList';
-  static const String drugInfoKey = 'pSgNVFtAyVKlcAgXHQCr2B%2FW9ePNujxYhU2vtnWIz1Ww%2BHArzjQ5586OqXmRYgnVvOfIdiXnBBsyEdC941PrIA%3D%3D'; // 업로드 시 삭제 요망!!!
+  static const String drugInfoKey = '-'; // 업로드 시 삭제 요망!!!
 }
